8293b77aefc67f7ac06c8167321b9053b486f731
claddblog <conan.ladd@gmail.com>
2025-04-15 13:04:12 -0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,3 @@
-// 🌐 claddorg-site: Clean starter for Cloudflare Pages
-// Includes Tailwind, Next.js pages for resume, papers, about
-
-// Folder structure (to go in GitHub repo):
-// /pages/index.tsx
-// /pages/resume.tsx
-// /pages/papers.tsx
-// /pages/about.tsx
-// /components/Header.tsx
-
 
 // --------------------------
 // components/Header.tsx
@@ -23,4 +13,3 @@ export default function Header() {
     </nav>
   );
 }
-
