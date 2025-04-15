fe212041f6fdc4bc06fa9978057e8504023f4fdd
claddblog <conan.ladd@gmail.com>
2025-04-15 10:31:15 -0700

    Update papers.tsx

--- pages/papers.tsx
+++ pages/papers.tsx
@@ -1,1 +1,18 @@
+// --------------------------
+// pages/papers.tsx
+import Header from '../components/Header';
 
+export default function Papers() {
+  return (
+    <main className="max-w-3xl mx-auto p-6">
+      <Header />
+      <h1 className="text-2xl font-bold mb-4">Research Papers</h1>
+      <ul className="list-disc ml-6 text-gray-700">
+        <li>
+          <span>Fractured Fabric (2024) – <em>PDF coming soon</em></span>
+        </li>
+        {/* Add more papers as needed */}
+      </ul>
+    </main>
+  );
+}
