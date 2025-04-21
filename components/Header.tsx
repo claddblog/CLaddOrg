f0e80e142616e79b3f72091516058ba12a509c3c
claddblog <conan.ladd@gmail.com>
2025-04-21 01:40:20 -0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,13 @@
-
-// --------------------------
-// components/Header.tsx
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <nav className="mb-8 space-x-4 text-gray-600">
-      <Link href="/">Home</Link>
-      <Link href="/resume">Resume</Link>
-      <Link href="/papers">Papers</Link>
-      <Link href="/about">About</Link>
-    </nav>
+    <header className="border-b py-4 mb-8 text-center">
+      <nav className="space-x-6 text-blue-700 text-sm font-medium">
+        <Link href="/">Home</Link>
+        <Link href="/resume">Resume</Link>
+        <Link href="/about">About</Link>
+      </nav>
+    </header>
   );
 }
